1ef4200a0af1f93b9738975f579d2745e4d2c72c
oioigohan <54663992+oioigohan@users.noreply.github.com>
2021-02-24 16:51:59 +0900

    Revert "Feature/counter"

--- src/app/directives/no-tag.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { NoTagDirective } from './no-tag.directive';
-
-describe('NoTagDirective', () => {
-  it('should create an instance', () => {
-    const directive = new NoTagDirective();
-    expect(directive).toBeTruthy();
-  });
-});
