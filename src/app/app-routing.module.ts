888dca7290905459e1c6ae188d5f27e219416522
oioigohan <oioigohan+github@gmail.com>
2021-02-24 17:40:14 +0900

    close #10 pathが空もしくは'/'でも'/counter'にリダイレクトするようにする

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { PageNotFoundPageComponent } from './features/error/components/pages/page-not-found-page/page-not-found-page.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: '/counter', pathMatch: 'full' },
   { path: 'counter', loadChildren: () => import('./features/counter/counter.module').then(m => m.CounterModule) },
   { path: '**', component: PageNotFoundPageComponent}
 ];
