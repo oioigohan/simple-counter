95bd68f2de5e8027ca5aa418cdfa5e17f1b71617
oioigohan <oioigohan+github@gmail.com>
2021-02-24 16:04:30 +0900

    NoTagDirectiveを追加

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DirectivesModule } from './directives/directives.module';
 
 @NgModule({
   declarations: [
@@ -11,6 +12,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule
+    DirectivesModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
