ef02f88a276d49e2571123ec33d06d134d11376a
oioigohan <oioigohan+github@gmail.com>
2021-02-24 17:21:20 +0900

    close #5 errorページの作成

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageNotFoundPageComponent } from './features/error/components/pages/page-not-found-page/page-not-found-page.component';
 
 const routes: Routes = [
   { path: 'counter', loadChildren: () => import('./features/counter/counter.module').then(m => m.CounterModule) },
+  { path: '**', component: PageNotFoundPageComponent}
 ];
 
 @NgModule({
--- src/app/features/error/error.module.ts
+++ src/app/features/error/error.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PageNotFoundPageComponent } from './components/pages/page-not-found-page/page-not-found-page.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [PageNotFoundPageComponent],
   imports: [
     CommonModule
   ]
